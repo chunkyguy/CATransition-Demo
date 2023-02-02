57547d72cf16699219e7abb6890a94efee0e9895
Sidharth Juyal <213683+chunkyguy@users.noreply.github.com>
2023-02-02 22:22:57 +0100

    Remove non-standard transitions

--- CATransitionDemo/ViewController.swift
+++ CATransitionDemo/ViewController.swift
@@ -8,83 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
-    //Class Variables
-    var transitions = ["oglFlip", "pageCurl", "pageUnCurl", "fade", "cube", "moveIn", "push", "reveal", "rippleEffect", "suckEffect", "cameraIris"];
-    var directions = [kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom];
-    var durations = [0.0, 0.15, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0];
+class ViewController: UIViewController {
     
-    var imageName = "imageA.jpg";
+    //Class Variables
+    private let transitions: [CATransitionType] = [.fade, .moveIn, .push, .reveal]
+    private let directions: [CATransitionSubtype] = [.fromLeft, .fromRight, .fromTop, .fromBottom]
+    private let durations = [0.0, 0.15, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0]
+    private let easing: [CAMediaTimingFunctionName] = [.linear, .easeIn, .easeOut, .easeInEaseOut]
+    private let images = ["imageA.jpg", "imageB.jpg"].compactMap(UIImage.init(named:))
+    private var currentImage = 0
     @IBOutlet var transitionPicker: UIPickerView!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var pickerView: UIPickerView!
     
     override func viewDidLoad() {
-        self.pickerView.delegate = self;
-        self.pickerView.dataSource = self;
-        
-        self.pickerView.selectRow(transitions.count / 2, inComponent: 0, animated: false);
-        self.pickerView.selectRow(directions.count / 2, inComponent: 1, animated: false);
-        self.pickerView.selectRow(durations.count / 2, inComponent: 2, animated: false);
+        imageView.image = images[currentImage]
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.selectRow(transitions.count / 2, inComponent: 0, animated: false)
+        pickerView.selectRow(1, inComponent: 1, animated: false)
+        pickerView.selectRow(2, inComponent: 2, animated: false)
+        pickerView.selectRow(2, inComponent: 3, animated: false)
     }
     
-    //UIPickerView DataSource method
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        //Three columns: type, direction, duration
-        return 3;
-    }
-    
-    //UIPickerView DataSource method
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        //Determine how many rows in each column
-        switch(component){
-            case 0:return transitions.count;
-            case 1: return directions.count;
-            case 2: return durations.count;
-            default: return 0;
-        }
-    }
-    
-    //UIPickerView DataSource method
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        //Determine the title of each row in each column
-        switch(component){
-            case 0:return transitions[row];
-            case 1: return directions[row];
-            case 2: return String(durations[row]) + "s";
-            default: return "";
-        }
-    }
-    
-    //This method is only necessary because the cameraIris transition sometimes does not go away after the animation is complete
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.view.layoutIfNeeded();
+    private func runAnimation(view: UIView) {
+        let animation = CATransition()
+        animation.type = transitions[pickerView.selectedRow(inComponent: 0)]
+        animation.subtype = directions[pickerView.selectedRow(inComponent: 1)]
+        animation.duration = durations[pickerView.selectedRow(inComponent: 2)]
+        animation.timingFunction = CAMediaTimingFunction(name: easing[pickerView.selectedRow(inComponent: 3)])
+        view.layer.add(animation, forKey: "fade-animation")
     }
     
     //Called when "View Transition" button tapped
-    @IBAction func showTransition(sender: UIButton) {
-        //Initialize the transition
-        let animation = CATransition();
+    @IBAction func showTransition() {
+        currentImage = (currentImage + 1) % images.count
+        imageView.image = images[currentImage]
+        runAnimation(view: imageView)
         
-        //Set transition properties
-        animation.type = transitions[pickerView.selectedRowInComponent(0)];
-        animation.subtype = directions[pickerView.selectedRowInComponent(1)];
-        animation.duration = durations[pickerView.selectedRowInComponent(2)];
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut);
-        
-        //Switch the image
-        if(self.imageName == "imageA.jpg"){
-            self.imageView.image = UIImage(named: "imageB.jpg");
-            self.imageName = "imageB.jpg";
-        }
-        else{
-            self.imageView.image = UIImage(named: "imageA.jpg");
-            self.imageName = "imageA.jpg";
-        }
-        
-        //Add transition to imageView, so that the entire view does not refresh
-        self.imageView.layer.addAnimation(animation, forKey: "animation");
+//        self.imageView.layer.add(animation, forKey: "animation")
     }
 }
 
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    //UIPickerView DataSource method
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //Determine how many rows in each column
+        switch(component){
+        case 0:return transitions.count
+        case 1: return directions.count
+        case 2: return durations.count
+        case 3: return easing.count
+        default: return 0
+        }
+    }
+    
+    //UIPickerView DataSource method
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //Determine the title of each row in each column
+        switch(component){
+        case 0: return transitions[row].rawValue
+        case 1: return directions[row].rawValue.lowercased().components(separatedBy: "from").joined()
+        case 2: return "\(durations[row])s"
+        case 3: return easing[row].rawValue.lowercased().components(separatedBy: "ease").joined(separator: " ")
+        default: return ""
+        }
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    //This method is only necessary because the cameraIris transition sometimes does not go away after the animation is complete
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    }
+}
